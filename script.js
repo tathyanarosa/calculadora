b292340663df6ae3ab93522662c94452778a0aff
Tathyana Rosa <81875337+tathyanarosa@users.noreply.github.com>
2021-04-04 17:41:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var valor;
+var resultado;
+function botao(num){
+    valor = document.calc.visor.value += num;
+}
+function resetar(){
+    document.calc.visor.value = '';
+}
+function calcular(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado.toLocaleString('pt-BR');
+}
